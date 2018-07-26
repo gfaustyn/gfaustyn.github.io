216c4436a3e6db0f076cc650fba7267f13e3027d
Gail Faustyn <gail.faustyn2@gmail.com>
2018-07-26 19:08:56 -0400

    updated javascript

--- javascript.js
+++ javascript.js
@@ -22,8 +22,8 @@ var captionText = document.getElementById("caption");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+   for (i = 0; i < dots.length; i++) {
+   dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
